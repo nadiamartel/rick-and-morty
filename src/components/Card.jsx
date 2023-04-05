97a43af8db1608e8713a1fdaf3fac46ffbd48f48
nadiamartel <nadiagmartel@gmail.com>
2023-04-04 21:32:28 -0300

    third commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,17 @@
 //import styles from "./Card.module.css"
-
-export default function Card({id, name, status, species, gender, origin, image, onClose}) {  
+import { Link } from "react-router-dom";
+   
+export default function Card({ id, name, status, species, gender, origin, image, onClose }) {
    return (
       <div>
-         <button onClick={() => onClose(id)}>X</button>
-         <h2>{name}</h2>
-         <h2>{status}</h2>
-         <h2>{species}</h2>
-         <h2>{gender}</h2>
-         <img src={image} alt="Foto de Rick" />
+            <button onClick={() => onClose(id)}>X</button>
+         <Link to={`/detail/${id}`}>
+            <h2>{name}</h2>
+         </Link>
+            <h2>{status}</h2>
+            <h2>{species}</h2>
+            <h2>{gender}</h2>
+            <img src={image} alt="Foto de Rick" />
       </div>
    )
 }
