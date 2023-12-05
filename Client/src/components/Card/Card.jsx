bfac80464dfd78cf97ef6be3932000d4a83b70f8
nadiamartel <nadiagmartel@gmail.com>
2023-12-05 19:41:18 -0300

    fix css

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 //import styles from "./Card.module.css"
 import { Link } from "react-router-dom";
 import style from "./Card.module.css";
-import { connect} from "react-redux";
+import { connect } from "react-redux";
 import { useState, useEffect } from "react";
 import { addFav, removeFav } from "../../redux/action";
 
@@ -33,22 +33,25 @@ function Card({ id, name, status, species, gender, origin, image, onClose, addFa
 
    return (
       <div className={style.divContenedor}>
-         {
-            isFav ? (
-               <button onClick={handleFavorite}>❤️</button>
-            ) : (
-               <button onClick={handleFavorite}>🤍</button>
-            )
-         }
-         <button onClick={() => onClose(id)}>X</button>
-         <Link to={`/detail/${id}`}>
-            <h2>{name}</h2>
-         </Link>
-         <img src={image} alt="Foto de Rick" className={style.img} />
-         <h2>{status}</h2>
-         <h2>{species}</h2>
-         <h2>{gender}</h2>
-
+         <div className={style.innerContenedor}>
+            <div className={style.button}>
+               {
+                  isFav ? (
+                     <button onClick={handleFavorite}>❤️</button>
+                  ) : (
+                     <button onClick={handleFavorite}>🤍</button>
+                  )
+               }
+               <button onClick={() => onClose(id)}>X</button>
+            </div>
+            <Link to={`/detail/${id}`}>
+               <h2>{name}</h2>
+            </Link>
+            <img src={image} alt="Foto del personaje" className={style.img} />
+            <h2>{status}</h2>
+            {/* <h2>{species}</h2>
+            <h2>{gender}</h2> */}
+         </div>
       </div>
    )
 }
@@ -64,13 +67,13 @@ const mapDispatchProps = (dispatch) => {
 };
 
 const mapStateToProps = (state) => {
-    return{
-       myFavorites : state.myFavorites,
-    }
- }
+   return {
+      myFavorites: state.myFavorites,
+   }
+}
 
 export default connect(mapStateToProps, mapDispatchProps)(Card);
 
 //hago destrunturing de todos los componentes que hay en card
-//<button onClick={() => onClose(id)}>X</button> 
+//<button onClick={() => onClose(id)}>X</button>
 //linea 55: conecta a dispatch con las funciones! creamos una prop q tenga el mismo nombre que la funcion
