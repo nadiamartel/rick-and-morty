93334e1024e405846f628b1ff2a4afe76c8898c6
nadiamartel <nadiagmartel@gmail.com>
2023-12-09 20:27:06 -0300

    fix about

--- Client/src/components/About/About.jsx
+++ Client/src/components/About/About.jsx
@@ -1,10 +1,36 @@
 const About = () => {
-    return(
+    return (
         <div>
-            <h2>Hola!! Soy Nadia, y vengo a contarles un poco acerca de mi ☺</h2>
-            <h3>Completar</h3>
 
-        </div>
+            <h1>Mi Página de Acerca de Mí</h1>
+
+            <main>
+                <section>
+                    <h2>Sobre Mí</h2>
+                    <p>Hola, soy Nadia. Soy bla. Disfruto bla. Me apasiona bla.</p>
+                    <p>Estoy emocionado/a por compartir más sobre mí y mis intereses contigo.</p>
+                </section>
+
+                <section>
+                    <h2>Mis Habilidades</h2>
+                    <ul>
+                        <li>Habilidad 1</li>
+                        <li>Habilidad 2</li>
+                        <li>Habilidad 3</li>
+                    </ul>
+                </section>
+
+                <section>
+                    <h2>Mi Experiencia</h2>
+                    <p>Tengo experiencia en bla. He trabajado en bla.</p>
+                </section>
+            </main>
+
+            <footer>
+                <p>Contacto: mail o redes</p>
+            </footer>
+
+        </div >
     )
 }
 
