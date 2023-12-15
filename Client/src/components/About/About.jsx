e96bc127538429eef5d744c4bba9e9df66b34faa
nadiamartel <nadiagmartel@gmail.com>
2023-12-15 20:53:42 -0300

    img ptte

--- Client/src/components/About/About.jsx
+++ Client/src/components/About/About.jsx
@@ -1,15 +1,16 @@
 import s from "./About.module.css"
+import img from "../img/profile_.jpeg"
 
 const About = () => {
     return (
         <div className={s.container}>
 
-            <h1 className={s.title}>Mi Página de Acerca de Mí</h1>
+            {/* <h1 className={s.title}>Mi Página de Acerca de Mí</h1> */}
 
             <main className={s.mainContent}>
                 <section className={s.section}>
                     <h2 className={s.sectionTitle}>Sobre Mí</h2>
-                    <img className={s.image} src="" alt="foto" />
+                    <img className={s.image} src={img} alt="foto" />
                     <p className={s.sectionText}>Hola, soy Nadia. Soy bla. Disfruto bla. Me apasiona bla.</p>
                     <p className={s.sectionText}>Estoy emocionado/a por compartir más sobre mí y mis intereses contigo.</p>
                 </section>
